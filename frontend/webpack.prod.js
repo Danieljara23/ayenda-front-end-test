7ca1a18d8b90b25bba29686676ea9760bb8e749f
Evinton Cordoba <evinton.antonio@gmail.com>
2021-12-16 17:17:30 -0500

    making deploying vercel

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,7 +1,12 @@
 const { merge } = require('webpack-merge');
 const webpackConfig = require('./webpack.config.js');
-
+const path = require('path');
 
 module.exports = merge(webpackConfig, {
-    mode:'production'
+    mode:'production',
+    "output": {
+        "path": path.resolve(__dirname, 'public/js/'),
+        "publicPath": "/",
+        "filename": "bundle.js"
+     }
 });

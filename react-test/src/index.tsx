914fc11b36efefb05c92618133862fab370e26e0
Óscar Córdoba <ocordoba3@hotmail.com>
2021-12-22 14:29:45 -0500

    First changes

---
+++ react-test/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HeroesApp } from './components/HeroesApp';
+
+ReactDOM.render( <HeroesApp />, document.getElementById('root') );

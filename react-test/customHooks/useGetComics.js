0d43236d025df21db81ecafdec95c5e0b9c4ccca
WilliamSantiagoAlzate <santiago_alza@hotmail.com>
2022-02-15 21:12:17 -0500

    Added env variables

--- react-test/customHooks/useGetComics.js
+++ react-test/customHooks/useGetComics.js
@@ -2,8 +2,8 @@ import {  useState, useEffect } from 'react';
 import { apiFetch } from '../helpers/apiFetch';
 import { formatComicsData } from '../helpers/formatComicsData';
 
-const url = 'https://gateway.marvel.com:443/v1/public/comics?'
-const apiKey = '12b9a96106dbadb12246ce164d96aff4'
+const url = process.env.NEXT_PUBLIC_API_URL;
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const useGetComics = () => {
   const [comics, setComics] = useState([]);
